f67d8884971c5f4da72f78f37da71e0d69292af3
Alison Reilly <reillyamr@gmail.com>
2018-12-05 19:33:14 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(array)
+ if array.length > 0
+  i = 0 
+  collection = []
+  while i < array.length 
+  
 
+  collection << yield(array[i])
+end
+  elsif array.length == 0
+  puts "Empty Array"
+ 
+  
+end 
+collection
+end 
 
